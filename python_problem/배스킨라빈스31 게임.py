f58e2c295c14ee6c87dcb223ae5c521737ea371b
03minseok <iris15977@gmail.com>
2025-01-03 01:34:15 +0900

    6단계

--- python_problem/배스킨라빈스31 게임.py
+++ python_problem/배스킨라빈스31 게임.py
@@ -1,41 +1,28 @@
 num=0
-inputNum = 0
-player='A'
 playerFlag=0
-
-while True:
-    inputNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-    if type(inputNum) != int:
-        print('정수를 입력하세요')
-        continue
-    if inputNum != 1 and inputNum != 2 and inputNum != 3:
-        print('1, 2, 3 중 하나를 입력하세요')
-        continue
-    break
-while inputNum>0:
+player = 'playerA'
+inputNum=0
+while num!=31:
+    while True:
+        inputNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if type(inputNum) != int:
+            print('정수를 입력하세요')
+            continue
+        if inputNum != 1 and inputNum != 2 and inputNum != 3:
+            print('1, 2, 3 중 하나를 입력하세요')
+            continue
+        break
+    while inputNum>0:
         num+=1
-        print(f"player{player} : {num}")
+        print(f"{player} : {num}")
         inputNum-=1
         if num==31:
             break
-playerFlag+=1
-if playerFlag%2==0:
-    player='A'
-else:
-    player='B'
+    if num==31:
+        break
+    playerFlag+=1
+    if playerFlag%2==0:
+        player='playerA'
+    else:
+        player='playerB'
 
-while True:
-    inputNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-    if type(inputNum) != int:
-        print('정수를 입력하세요')
-        continue
-    if inputNum != 1 and inputNum != 2 and inputNum != 3:
-        print('1, 2, 3 중 하나를 입력하세요')
-        continue
-    break
-while inputNum>0:
-        num+=1
-        print(f"player{player} : {num}")
-        inputNum-=1
-        if num==31:
-            break
