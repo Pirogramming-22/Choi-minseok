f7eeb8773010239c6fa6c5dde321ad0e856a96c8
03minseok <iris15977@gmail.com>
2025-01-03 01:26:46 +0900

    3단계

--- python_problem/배스킨라빈스31 게임.py
+++ python_problem/배스킨라빈스31 게임.py
@@ -1,29 +1,11 @@
 num=0
-playerFlag=0
-player = 'playerA'
-inputNum=0
-while num!=31:
-    while True:
-        inputNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-        if type(inputNum) != int:
-            print('정수를 입력하세요')
-            continue
-        if inputNum != 1 and inputNum != 2 and inputNum != 3:
-            print('1, 2, 3 중 하나를 입력하세요')
-            continue
-        break
-    while inputNum>0:
-        num+=1
-        print(f"{player} : {num}")
-        inputNum-=1
-        if num==31:
-            break
-    if num==31:
-        print(f'{player} win!')
-        break
-    playerFlag+=1
-    if playerFlag%2==0:
-        player='playerA'
-    else:
-        player='playerB'
-
+inputNum = 0
+while True:
+    inputNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+    if type(inputNum) != int:
+        print('정수를 입력하세요')
+        continue
+    if inputNum != 1 and inputNum != 2 and inputNum != 3:
+        print('1, 2, 3 중 하나를 입력하세요')
+        continue
+    break
